e3da46ef3d801a6009374c9af43d1b2b513b97ac
Steven Ford <evensteven01@gmail.coma>
2024-11-09 15:01:03 -0500

    Add some practice challenges

--- practice/test_models.py
+++ practice/test_models.py
@@ -18,3 +18,33 @@ def test_find_figure_at_x_0():
     
     # Assert
     assert exp_fig == act_fig
+
+
+def test_find_I_figure():
+    """
+        Make a list of 3 figures, and find the one of type "I".
+        Follow the Arrange, Act, Assert pattern as in test_find_figure_at_x_0.
+    """
+    ...
+    
+def test_find_I_figure():
+    """
+        Make a list of 3 figures, two of them should be active, and find the ones that are active.
+        Follow the Arrange, Act, Assert pattern as in test_find_figure_at_x_0.
+        
+    """
+    # Arrange
+    exp_active_ct = 2
+
+    # Act
+    act_active = 0
+
+    # Assert
+    assert act_active == exp_active_ct 
+
+
+def test_initFigure_I():
+    """
+        Test creating an I_TETROMINO figure. Make sure the created figure has 4 items in its coordList.
+    """
+    ...

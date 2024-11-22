2ef6e521dbaa3f0509fb26213fc3a1d14ee1de20
Ryan Setaruddin <coolry1998@gmail.com>
2024-11-22 17:01:56 -0500

    pass 3 tests but 1 failed.

--- practice/test_models.py
+++ practice/test_models.py
@@ -11,6 +11,7 @@ def test_find_figure_at_x_0():
 
     # Act
     act_fig = None
+
     for fig in figs:
         if fig.x == 0:
             act_fig = fig
@@ -20,7 +21,7 @@ def test_find_figure_at_x_0():
     assert exp_fig == act_fig
 
 
-def test_find_I_figure():
+def test_find_type_I_figure():
     """
         Make a list of 3 figures, and find the one of type "I".
         Follow the Arrange, Act, Assert pattern as in test_find_figure_at_x_0.
@@ -35,29 +36,33 @@ def test_find_I_figure():
 
     # Act
     act_fig = None
+
     for fig in figs:
-        if fig.x == 1:
+        if fig.type == "I":
             act_fig = fig
-            break
 
     # Assert
     assert exp_fig == act_fig
     
-def test_find_I_figure():
+def test_find_active_figure():
     """
         Make a list of 3 figures, two of them should be active, and find the ones that are active.
         Follow the Arrange, Act, Assert pattern as in test_find_figure_at_x_0.
     """
     # Arrange
     fig1 = Figure(2, 0, 1, "S", True)
-    fig2 = Figure(2, 1, 0, "J", True)
-    fig3 = Figure(0, 0, 1, "T")
+    fig2 = Figure(0, 1, 0, "J", True)
+    fig3 = Figure(0, 0, 1, "T", False)
     figs = [fig1, fig2, fig3]
 
     exp_active_ct = 2
 
     # Act
     act_active = 0
+
+    for fig in figs:
+        if fig.isActive:
+            act_active += 1
 
     # Assert
     assert act_active == exp_active_ct 
@@ -68,8 +73,12 @@ def test_initFigure_I():
         Test creating an I_TETROMINO figure. Make sure the created figure has 4 items in its coordList.
     """
     # Arrange
-    coordList = Figure(0, 1, "I", True)
+    fig = Figure(0, 1, "I", True)
     
     # Act
+    exp_fig = list()
+
+    act_fig = None
 
     # Asset
+    assert exp_fig == act_fig

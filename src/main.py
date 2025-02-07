60e2ac52a33a5851a14b3258a1223a188ef55e78
Ryan Setaruddin <coolry1998@gmail.com>
2025-02-07 16:04:12 -0500

    moved game loop into tetrisGame py file

--- src/main.py
+++ src/main.py
@@ -30,142 +30,16 @@ def main():
     # initialize pygame
     pygame.init()
 
+    game_width, game_height = 20, 25
+    grid_location_x, grid_location_y = 5, 5
+
     # Size in grid
     grid_width, grid_height, square_size = 10, 20, 50
-    size = ((grid_width+10)*square_size, grid_height*square_size)
+    tetris = TetrisGame(game_width, game_height, grid_location_x, grid_location_y,
+        grid_width, grid_height, square_size)
 
-    game_paused, menu_state = False, "main"
-    font = pygame.font.SysFont("arial", 40)
-    TEXT_COL = (255, 255, 255)
+    tetris.start()
 
-    # screen window
-    screen = pygame.display.set_mode((size))
-
-    # title & caption
-    pygame.display.set_caption('Tetris')
-
-    # font variables
-    font = pygame.font.SysFont('arial', 40)
-
-    # # load images
-    # path = "src/Button/Images/"
-    # start_img = pygame.image.load(os.path.join(os.getcwd(), path, "start_btn.png"))
-    # exit_img = pygame.image.load(os.path.join(os.getcwd(), path, "exit_btn.png"))
-    # resume_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_resume.png"))
-    # options_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_options.png"))
-    # quit_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_quit.png"))
-    # back_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_back.png"))
-
-    # # create button instances
-    # start_button = Button(200, 200, start_img, 0.8)
-    # exit_button = Button(200, 200, exit_img, 0.8)
-    # resume_button = Button(304, 125, resume_img, 1)
-    # options_button = Button(297, 250, options_img, 1)
-    # quit_button = Button(336, 375, quit_img, 1)
-    # back_button = Button(332, 450, back_img, 1)
-            
-    # instance
-    game = TetrisGame(grid_width, grid_height, square_size)
-
-    # game variables
-    running = True
-    game_tick_freq = 3
-    turn = 0
-
-     # game loop
-    while running:
-        moved = False
-        screen.fill((52, 78, 91))
-
-        # call draw
-        game.draw(screen)
-
-        if game.state is GAMESTATE.GAMEOVER:
-            print("Game over")
-            break
-
-        # call move
-        if turn % game_tick_freq == 0:
-            game.move()
-            moved = True
-
-        # call deactive 
-        game.deactivate()
-
-        # call new figures
-        if game.getActiveFigure() is None:
-            game.newFigure()
-
-        # check if game is paused
-        if game_paused == True:
-            # check menu state
-            if menu_state == "main":
-                # # check buttons has been drawn
-                # if start_button.draw(screen):
-                #      # call draw
-                #     game.draw(screen)
-
-                #     # call move
-                #     game.move()
-
-                # if exit_button.draw(screen):
-                #     # quit
-                #     pygame.quit()
-                #     sys.exit(0)
-                pass
-
-                # draw pause screen buttons
-                if resume_button.draw(screen):
-                    game_paused = False
-
-                if options_button.draw(screen):
-                    menu_state = "options"
-
-                # if quit_button.draw(screen):
-                #     running = False
-                #     # quit
-                #     pygame.quit()
-                #     sys.exit(0)
-
-            # check if the options menu is open
-            if menu_state == "options":
-                if back_button.draw(screen):
-                    menu_state = "main"
-        
-        # event handler
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.type == QUIT:
-                    # quit
-                    running = False
-                    pygame.quit()
-                    sys.exit(0)
-                if event.key == K_DOWN:
-                    game.downMove()
-                    moved = True
-                if event.key == K_LEFT:
-                    game.leftMove()
-                    moved = True
-                if event.key == K_RIGHT:
-                    game.rightMove()
-                    moved = True
-                if event.key == K_SPACE:
-                    game.rotate()
-                    moved = True
-                if event.key == K_ESCAPE:
-                    game_paused = True
-
-        time.sleep(.1)
-        turn += 1
-        if moved:
-            game.updateGrid()
-
-        game.checkGameState()
-
-        #game.clearFullRows()
-
-        # pygame update
-        pygame.display.update()
 
 # run the main program
 if __name__ == '__main__':
--- src/models/TetrisGame.py
+++ src/models/TetrisGame.py
@@ -18,16 +18,24 @@ class TetrisGame:
     currentFigureIndex = 0
 
     """constructor"""
-    def __init__(self, width, height, square_size):
+    def __init__(self, game_width, game_height, grid_location_x, grid_location_y,
+        grid_width, grid_height, square_size
+    ):
         self.level, self.score = 2, 0
         self.state = GAMESTATE.RUNNING
-        self.height, self.width = height, width
-        self.velocity = 1
-        self.figures, self.nextFigures = [], []
+
+        self.height, self.width = game_height, game_width
+        self.grid_location_x, self.grid_location_y = grid_location_x, grid_location_y
+        self.grid_width, self.grid_height = grid_width, grid_height
         self.square_size = square_size
+
+        self.figures, self.nextFigures = [], []
+        self.velocity = 1
+
         self.initFigures()
+
         # Grid is by y then x. So grid[y][x]
-        self.grid = [[None for i in range(width)] for j in range(height)]
+        self.grid = [[None for i in range(self.grid_width)] for j in range(self.grid_height)]
 
     @classmethod
     def _getNextFigureId(cls) -> int:
@@ -36,20 +44,20 @@ class TetrisGame:
 
 
     def initFigures(self):
-        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.width, self.height, self.square_size))
-        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.width, self.height, self.square_size))
-        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.width, self.height, self.square_size))
-        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.width, self.height, self.square_size))
+        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.grid_width, self.grid_height, self.square_size))
+        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.grid_width, self.grid_height, self.square_size))
+        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.grid_width, self.grid_height, self.square_size))
+        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.grid_width, self.grid_height, self.square_size))
 
     # new figure method
     def newFigure(self):
         nextFig: Figure = self.nextFigures.pop()
 
-        nextFig.setX(self.width//2)
-        nextFig.setY(-4)
+        nextFig.setX(self.grid_width//2)
+        nextFig.setY(self.grid_height-4)
         nextFig.isActive = True
 
-        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.width, self.height, self.square_size))
+        self.nextFigures.append(Figure.getRandomFigure(self._getNextFigureId(), self.grid_width, self.grid_height, self.square_size))
 
         self.figures.append(nextFig)
 
@@ -60,7 +68,7 @@ class TetrisGame:
             
     def updateGrid(self):
         # Reset the grid
-        self.grid = [[None for i in range(self.width)] for j in range(self.height)]
+        self.grid = [[None for i in range(self.grid_width)] for j in range(self.grid_height)]
         # Add each figure's coords to the grid
         for fig in self.figures:
             for coord in fig.coordList:
@@ -119,7 +127,7 @@ class TetrisGame:
 
             while empty:
                 spacesUnder += 1
-                if y+spacesUnder >= self.height:
+                if y+spacesUnder >= self.grid_height:
                     break
 
                 if self.grid[y+spacesUnder][x] is None or self.grid[y+spacesUnder][x] is activeFigure:
@@ -144,7 +152,7 @@ class TetrisGame:
     def move(self):
         for fig in self.figures:
             if fig.state == "start":
-                spaceLeft = self.height - fig.getBottom() - 1
+                spaceLeft = self.grid_height - fig.getBottom() - 1
                 enoughSpace = self.velocity < spaceLeft
                 moveAmount = self.velocity if enoughSpace else spaceLeft
 
@@ -153,7 +161,7 @@ class TetrisGame:
                 else:
                     fig.setY(fig.y+moveAmount)
 
-            if fig.getBottom() == self.height -1:
+            if fig.getBottom() == self.grid_height -1:
                 fig.state = "stop"
 
     # leftMove method
@@ -171,8 +179,8 @@ class TetrisGame:
     # rightMove method
     def rightMove(self):
         for fig in self.figures:
-            if fig.isActive and fig.getRight() <= self.width:
-                moveAmount = 1 if (self.width - fig.getRight()) > 1 else 0
+            if fig.isActive and fig.getRight() <= self.grid_width:
+                moveAmount = 1 if (self.grid_width - fig.getRight()) > 1 else 0
 
                 if self.willCollide(fig=fig, dx=moveAmount, dy=0):
                     continue
@@ -182,8 +190,8 @@ class TetrisGame:
     # downMove method
     def downMove(self):
         for fig in self.figures:
-            if fig.isActive and fig.getBottom() <= self.height:
-                spaceLeft = self.height - fig.getBottom() - 1
+            if fig.isActive and fig.getBottom() <= self.grid_height:
+                spaceLeft = self.grid_height - fig.getBottom() - 1
                 enoughSpace = self.velocity < spaceLeft
                 moveAmount = self.velocity if enoughSpace else spaceLeft
 
@@ -192,7 +200,7 @@ class TetrisGame:
                 else:
                     fig.setY(fig.y+moveAmount)
 
-            if fig.getBottom() == self.height -1:
+            if fig.getBottom() == self.grid_height -1:
                 fig.state = "stop"
 
     # rotate method
@@ -220,3 +228,137 @@ class TetrisGame:
             if shapeAtCoord and not thisFigAtCoord:
                 return True
         return False
+
+    def start(self):
+        
+        size = ((self.grid_width+10)*self.square_size, self.grid_height*self.square_size)
+
+        game_paused, menu_state = False, "main"
+        font = pygame.font.SysFont("arial", 40)
+        TEXT_COL = (255, 255, 255)
+
+        # screen window
+        screen = pygame.display.set_mode((size))
+
+        # title & caption
+        pygame.display.set_caption('Tetris')
+
+        # font variables
+        font = pygame.font.SysFont('arial', 40)
+
+        # # load images
+        # path = "src/Button/Images/"
+        # start_img = pygame.image.load(os.path.join(os.getcwd(), path, "start_btn.png"))
+        # exit_img = pygame.image.load(os.path.join(os.getcwd(), path, "exit_btn.png"))
+        # resume_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_resume.png"))
+        # options_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_options.png"))
+        # quit_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_quit.png"))
+        # back_img = pygame.image.load(os.path.join(os.getcwd(), path, "button_back.png"))
+
+        # # create button instances
+        # start_button = Button(200, 200, start_img, 0.8)
+        # exit_button = Button(200, 200, exit_img, 0.8)
+        # resume_button = Button(304, 125, resume_img, 1)
+        # options_button = Button(297, 250, options_img, 1)
+        # quit_button = Button(336, 375, quit_img, 1)
+        # back_button = Button(332, 450, back_img, 1)
+            
+        # game variables
+        running = True
+        game_tick_freq = 3
+        turn = 0
+
+         # game loop
+        while running:
+            moved = False
+            screen.fill((52, 78, 91))
+
+            # call draw
+            self.draw(screen)
+
+            if self.state is GAMESTATE.GAMEOVER:
+                print("Game over")
+                break
+
+            # call move
+            if turn % game_tick_freq == 0:
+                self.move()
+                moved = True
+
+            # call deactive 
+            self.deactivate()
+
+            # call new figures
+            if self.getActiveFigure() is None:
+                self.newFigure()
+
+            # check if game is paused
+            if game_paused == True:
+                # check menu state
+                if menu_state == "main":
+                    # # check buttons has been drawn
+                    # if start_button.draw(screen):
+                    #      # call draw
+                    #     self.draw(screen)
+
+                    #     # call move
+                    #     self.move()
+
+                    # if exit_button.draw(screen):
+                    #     # quit
+                    #     pygame.quit()
+                    #     sys.exit(0)
+                    pass
+
+                    # draw pause screen buttons
+                    if resume_button.draw(screen):
+                        game_paused = False
+
+                    if options_button.draw(screen):
+                        menu_state = "options"
+
+                    # if quit_button.draw(screen):
+                    #     running = False
+                    #     # quit
+                    #     pygame.quit()
+                    #     sys.exit(0)
+
+                # check if the options menu is open
+                if menu_state == "options":
+                    if back_button.draw(screen):
+                        menu_state = "main"
+            
+            # event handler
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.type == QUIT:
+                        # quit
+                        running = False
+                        pygame.quit()
+                        sys.exit(0)
+                    if event.key == K_DOWN:
+                        self.downMove()
+                        moved = True
+                    if event.key == K_LEFT:
+                        self.leftMove()
+                        moved = True
+                    if event.key == K_RIGHT:
+                        self.rightMove()
+                        moved = True
+                    if event.key == K_SPACE:
+                        self.rotate()
+                        moved = True
+                    if event.key == K_ESCAPE:
+                        game_paused = True
+
+            time.sleep(.1)
+            turn += 1
+            if moved:
+                self.updateGrid()
+
+            self.checkGameState()
+
+            #self.clearFullRows()
+
+            # pygame update
+            pygame.display.update()
